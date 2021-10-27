d490ecdebcf9359762dc8aa8b6c78bf9374e52d2
StephaneMercier <stefane.mercier@gmail.com>
2021-10-27 16:04:59 +0200

    Color selection loop done

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,1 +1,31 @@
-let myCart = [product._id, product.total, product.color];
+const cart = [
+  {
+    productId: "abc123",
+    name: "Kanap Test",
+    image: "http://....",
+    unitPrice: 1880,
+    quantity: 1,
+  },
+  {
+    productId: "abc123bleu",
+    name: "Kanap Test 2 Bleu",
+    image: "http://....",
+    unitPrice: 2900,
+    quantity: 2,
+  },
+];
+
+// const total = 0;
+// let myCart = [product._id, product.total, product.color];
+
+function addItem() {}
+
+function removeItem() {}
+
+function updateQty() {}
+
+function clearCart() {}
+
+function getCart() {
+  return cart;
+}
--- front/js/product.js
+++ front/js/product.js
@@ -1,46 +1,56 @@
-// let product = fetch(`http://localhost:3000/api/products/`);
-// console.log(product);
-// const productUrl = new URLSearchParams(product);
-// var productId = productUrl.has("id");
-// console.log(productId);
-
 // Récupération de l'ID du produit à afficher
 
-let productUrl = window.location.search;
-let urlParams = new URLSearchParams(productUrl);
-let productId = urlParams.get("id");
-console.log(productId);
+function getProductIdByUrlParam() {
+  let productUrl = window.location.search;
+  let urlParams = new URLSearchParams(productUrl);
+  let productId = urlParams.get("id");
+  return productId;
+}
 
 // Appel du produit spécifique
 
 async function fetchProductById(productId) {
   const res = await fetch(`http://localhost:3000/api/products/${productId}`);
+  // console.log(res);
+  // console.log(productId);
   const productRes = await res.json();
+  // console.log(productRes);
+
   return productRes;
 }
 
 // Insertion dynamique des détails du produit
 
-function createHtmlProductDetails(product) {
+async function populateHtmlProductDetails(product) {
   document.querySelector(
     ".item__img"
   ).innerHTML = `<img src="${product.imageUrl}" alt= "${product.altTxt}" />`;
   document.getElementById("title").innerText = product.name;
   document.getElementById("description").innerText = product.description;
   document.getElementById("price").innerText = product.price + " ";
-  document.querySelector(
-    "#colors"
-  ).innerHTML = `<option value="${product.colors}"></option>`;
+
+  // Affichage du nom du produit dans l'onglet de la page
+  document.title = `${product.name} | KANAP `;
+
+  let removeOption = document.querySelector(`option`);
+  let selectColor = document.getElementById("colors");
+  selectColor.removeChild(removeOption);
+  let colors = product.colors;
+
+  // boucle dans le tableau des "colors" et incrémentation des <option></option> pour chaque produit
+  colors.forEach((color) => {
+    selectColor.innerHTML += `<option value="${color}">${color}</option>`;
+  });
 }
 
 // Affichage du produit avec ses détails
 
-async function displayProduct() {
+async function displayProduct(productId) {
   let product = await fetchProductById(productId);
-  console.log(product);
-  createHtmlProductDetails(product);
+  populateHtmlProductDetails(product);
 }
 
 (async () => {
-  await displayProduct();
+  let productId = getProductIdByUrlParam();
+  await displayProduct(productId);
 })();

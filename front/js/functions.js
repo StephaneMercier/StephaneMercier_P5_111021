27a56e2d52a9912445efb8a58ed0b6e9836c61aa
StephaneMercier <stefane.mercier@gmail.com>
2021-12-11 12:56:56 +0100

    Minopr fixes

--- front/js/functions.js
+++ front/js/functions.js
@@ -16,19 +16,3 @@ function fetchCartFromLocalStorage() {
 function cartToLocalStorage(cart) {
   localStorage.setItem("cart", JSON.stringify(cart));
 }
-
-// Fonction globale pour validation du formulaire
-function validate(id, pattern, messageError) {
-  const $field = document.querySelector(`#${id}`);
-  const $errorMsg = document.querySelector(`#${id}ErrorMsg`);
-  const fieldValue = $field.value.trim();
-  const regex = `/^[${pattern}]+$/`;
-
-  if (fieldValue == "" || !regex.test(fieldValue)) {
-    $errorMsg.textContent = messageError;
-    return false;
-  } else {
-    $errorMsg.textContent = "";
-    return fieldValue;
-  }
-}
